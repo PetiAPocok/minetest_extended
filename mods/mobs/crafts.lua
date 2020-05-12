72c1fd3bebc8a4b949a9f8132c4a4fd3b74faf1a
PetiAPocok <mmandna@gmail.com>
2020-05-12 17:16:02 +0200

    Removed net

--- mods/mobs/crafts.lua
+++ mods/mobs/crafts.lua
@@ -66,24 +66,6 @@ end
 
 minetest.register_alias("mobs:magic_lasso", "mobs:lasso")
 
--- net
-minetest.register_tool("mobs:net", {
-	description = S("Net (right-click animal to put in inventory)"),
-	inventory_image = "mobs_net.png",
-	groups = {flammable = 2}
-})
-
-if minetest.get_modpath("farming") then
-	minetest.register_craft({
-		output = "mobs:net",
-		recipe = {
-			{"group:stick", "", "group:stick"},
-			{"group:stick", "", "group:stick"},
-			{"farming:string", "group:stick", "farming:string"}
-		}
-	})
-end
-
 -- shears (right click to shear animal)
 minetest.register_tool("mobs:shears", {
 	description = S("Steel Shears (right-click to shear)"),
