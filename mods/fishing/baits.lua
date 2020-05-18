f5734d5f79660621103c1c7e32e582589420354e
PetiAPocok <mmandna@gmail.com>
2020-05-18 21:04:37 +0200

    added worm dropp to dirt

--- mods/fishing/baits.lua
+++ mods/fishing/baits.lua
@@ -4,13 +4,12 @@ minetest.register_craftitem("fishing:worm", {
     groups = { fishing_bait = 1 },
 })
 
--- expanded_groups = minetest.registered_items["farming:corn"].groups
--- expanded_groups["fishing_bait"] = 1
--- { items = {'farming:peanut'}, rarity = 20 }
--- minetest.override_item("farming:corn", {
--- 	groups = expanded_groups
--- })
-
+-- Add worm drop to dirt drops
+expanded_drop = minetest.registered_items["default:dirt"].drop.items
+table.insert(expanded_drop, { rarity = 30, items = {"fishing:worm"} })
+minetest.override_item("farming:corn", {
+    drop = expanded_drop
+})
 
 -- Make corn useable as fishing bait
 local expanded_groups = minetest.registered_items["farming:seed_corn"].groups
