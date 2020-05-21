d7be4c4846006da921b9414218d1ea829412333c
PetiAPocok <mmandna@gmail.com>
2020-05-21 10:21:08 +0200

    fixed name

--- mods/hunger_ng/interoperability/fishing.lua
+++ mods/hunger_ng/interoperability/fishing.lua
@@ -1,7 +1,7 @@
 local add = hunger_ng.add_hunger_data
 
 add('fishing:clownfish',                                         { satiates = 1, heals= -1 })
-add('fishing:fish_cooked',                                           { satiates = 4 })
-add('fishing:tuna',                                              { satiates = 2, heals= -1 })
 add('fishing:salmon',                                            { satiates = 2, heals= -1 })
-add('fishing:salmon_cooked',                                         { satiates = 4 })
+add('fishing:salmon_cooked',                                     { satiates = 4 })
+add('fishing:tuna',                                              { satiates = 2, heals= -1 })
+add('fishing:tuna_cooked',                                       { satiates = 4 })
