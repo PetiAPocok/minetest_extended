f6f34332bc5188b376a0848e216fedb2893e6264
PetiAPocok <mmandna@gmail.com>
2020-06-01 11:27:20 +0200

    removed admin set

--- mods/3d_armor/3d_armor/armor.lua
+++ mods/3d_armor/3d_armor/armor.lua
@@ -1,55 +1,6 @@
 -- support for i18n
 local S = armor_i18n.gettext
 
-armor:register_armor("3d_armor:helmet_admin", {
-	description = S("Admin Helmet"),
-	inventory_image = "3d_armor_inv_helmet_admin.png",
-	armor_groups = {fleshy=100},
-	groups = {armor_head=1, armor_heal=100, armor_use=0, armor_water=1,
-			not_in_creative_inventory=1},
-	on_drop = function(itemstack, dropper, pos)
-		return
-	end,
-})
-
-armor:register_armor("3d_armor:chestplate_admin", {
-	description = S("Admin Chestplate"),
-	inventory_image = "3d_armor_inv_chestplate_admin.png",
-	armor_groups = {fleshy=100},
-	groups = {armor_torso=1, armor_heal=100, armor_use=0,
-			not_in_creative_inventory=1},
-	on_drop = function(itemstack, dropper, pos)
-		return
-	end,
-})
-
-armor:register_armor("3d_armor:leggings_admin", {
-	description = S("Admin Leggings"),
-	inventory_image = "3d_armor_inv_leggings_admin.png",
-	armor_groups = {fleshy=100},
-	groups = {armor_legs=1, armor_heal=100, armor_use=0,
-			not_in_creative_inventory=1},
-	on_drop = function(itemstack, dropper, pos)
-		return
-	end,
-})
-
-armor:register_armor("3d_armor:boots_admin", {
-	description = S("Admin Boots"),
-	inventory_image = "3d_armor_inv_boots_admin.png",
-	armor_groups = {fleshy=100},
-	groups = {armor_feet=1, armor_heal=100, armor_use=0,
-			not_in_creative_inventory=1},
-	on_drop = function(itemstack, dropper, pos)
-		return
-	end,
-})
-
-minetest.register_alias("adminboots", "3d_armor:boots_admin")
-minetest.register_alias("adminhelmet", "3d_armor:helmet_admin")
-minetest.register_alias("adminchestplate", "3d_armor:chestplate_admin")
-minetest.register_alias("adminleggings", "3d_armor:leggings_admin")
-
 if armor.materials.wood then
 	armor:register_armor("3d_armor:helmet_wood", {
 		description = S("Wood Helmet"),
