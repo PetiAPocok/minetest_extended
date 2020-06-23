7f1fabd057db741a2c0ea5e3d5d89077e9068577
PetiAPocok <mmandna@gmail.com>
2020-06-23 12:38:22 +0200

    cleaning

--- mods_for_research/hunger_ng_for_bugfixes/interoperability/fishing.lua
+++
@@ -1,7 +0,0 @@
-local add = hunger_ng.add_hunger_data
-
-add('fishing:clownfish',                                         { satiates = 1, heals= -1 })
-add('fishing:salmon',                                            { satiates = 2, heals= -1 })
-add('fishing:salmon_cooked',                                     { satiates = 4 })
-add('fishing:tuna',                                              { satiates = 2, heals= -1 })
-add('fishing:tuna_cooked',                                       { satiates = 4 })
