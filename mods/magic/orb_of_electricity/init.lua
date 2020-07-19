6fe7912c0bd061880bc2703a6664ce5efb84d63f
PetiAPocok <mironm1996@gmail.com>
2020-07-19 13:40:53 +0200

    changed lightning projectile and made it caster friendly

--- mods/magic/orb_of_electricity/init.lua
+++ mods/magic/orb_of_electricity/init.lua
@@ -1,3 +1,6 @@
+local ray_count = 0
+local rays = {}
+
 minetest.register_craftitem("orb_of_electricity:orb_of_electricity", {
     description = "Orb of Electricity\nMana: 10",
     inventory_image = "orb_of_electricity_orb.png",
@@ -10,18 +13,36 @@ minetest.register_craftitem("orb_of_electricity:orb_of_electricity", {
 
             local player_pos = player:get_pos()
             local dir = player:get_look_dir()
+            local objs = {}
+            local loop_counter = 1
 
-            obj = minetest.add_entity({
-                x = player_pos.x,
-                y = player_pos.y + 1.5,
-                z = player_pos.z
-            }, "orb_of_electricity:lightning_ball")
+            while loop_counter < 30 do
+                local entity_pos = {
+                    x = player_pos.x + loop_counter * dir.x / 14.5,
+                    y = player_pos.y + 1.4 + loop_counter * dir.y / 14.5,
+                    z = player_pos.z + loop_counter * dir.z / 14.5
+                }
 
-            obj:setvelocity({
-                x = dir.x * 30,
-                y = dir.y * 30,
-                z = dir.z * 30
-            })
+                if minetest.get_node(entity_pos).name == "air" then
+                    objs[loop_counter] = minetest.add_entity(entity_pos, "orb_of_electricity:lightning")
+
+                    objs[loop_counter]:set_rotation({
+                        x = -player:get_look_vertical(),
+                        y = player:get_look_horizontal(),
+                        z = 0
+                    })
+
+                    objs[loop_counter]:get_luaentity()._id = ray_count
+                    objs[loop_counter]:get_luaentity()._owner = name
+                else
+                    loop_counter = 30
+                end
+
+                loop_counter = loop_counter + 1
+            end
+
+            rays[ray_count] = objs
+            ray_count = ray_count + 1
         end
     end
 })
@@ -35,16 +56,19 @@ minetest.register_craft({
 	}
 })
 
-minetest.register_entity("orb_of_electricity:lightning_ball", {
+minetest.register_entity("orb_of_electricity:lightning", {
     physical = false,
-    visual = "sprite",
-    visual_size = {x=0.5, y=0.5},
-    textures = {"orb_of_electricity_lightning_ball.png"},
-    collisionbox = {-0.1,-0.1,-0.1,0.1,0.1,0.1},
+    visual = "mesh",
+    mesh = "orb_of_electricity_lightning.obj",
+    textures = {"orb_of_electricity_lightning.png"},
+    collisionbox = {-0.01,-0.01,-0.07,0.01,0.01,0.01},
     pointable = false,
-    timer = 0,
+    visual_size = {x=1, y=1, z=1},
+    _timer = 0,
+    _id = 0,
+    _owner = "",
     on_step = function(self, dtime)
-        self.timer = self.timer + dtime
+        self._timer = self._timer + dtime
         local pos = self.object:get_pos()
         local node = minetest.get_node(pos)
 
@@ -52,49 +76,47 @@ minetest.register_entity("orb_of_electricity:lightning_ball", {
             self.object:remove()
         end
 
-        minetest.add_particlespawner({
-            amount = 10,
-            time = 0.10,
-            minpos = {
-                x = pos.x - 0.1,
-                y = pos.y - 0.1,
-                z = pos.z - 0.1
-            },
-            maxpos = {
-                x = pos.x + 0.1,
-                y = pos.y + 0.1,
-                z = pos.z + 0.1
-            },
-            minexptime = 0.1,
-            maxexptime = 0.2,
-            minsize = 0.50,
-            maxsize = 0.50,
-            collisiondetection = false,
-            vertical = false,
-            texture = "orb_of_electricity_particle.png",
-        })
+        -- minetest.add_particlespawner({
+        --     amount = 10,
+        --     time = 0.10,
+        --     minpos = {
+        --         x = pos.x - 0.1,
+        --         y = pos.y - 0.1,
+        --         z = pos.z - 0.1
+        --     },
+        --     maxpos = {
+        --         x = pos.x + 0.1,
+        --         y = pos.y + 0.1,
+        --         z = pos.z + 0.1
+        --     },
+        --     minexptime = 0.1,
+        --     maxexptime = 0.2,
+        --     minsize = 0.50,
+        --     maxsize = 0.50,
+        --     collisiondetection = false,
+        --     vertical = false,
+        --     texture = "orb_of_electricity_particle.png",
+        -- })
 
-        if self.timer > 0.2 then
-            local objs = minetest.get_objects_inside_radius({x=pos.x,y=pos.y,z=pos.z}, 2)
-            for k, obj in pairs(objs) do
-                if obj:get_luaentity() ~= nil then
-                    if obj:get_luaentity().name ~= "orb_of_electricity:lightning_ball" and obj:get_luaentity().name ~= "__builtin:item" then
-                        obj:punch(self.object, 1.0, {
-                            full_punch_interval = 1.0,
-                            damage_groups = {fleshy = 3},
-                        }, nil)
-
-                        self.object:remove()
-                    end
-                elseif obj:is_player() then
+        local objs = minetest.get_objects_inside_radius({x=pos.x,y=pos.y,z=pos.z}, 1.5)
+        for k, obj in pairs(objs) do
+            if obj:get_luaentity() ~= nil then
+                if obj:get_luaentity().name ~= "orb_of_electricity:lightning" and obj:get_luaentity().name ~= "__builtin:item" then
                     obj:punch(self.object, 1.0, {
                         full_punch_interval = 1.0,
                         damage_groups = {fleshy = 3},
                     }, nil)
-                    self.object:remove()
                 end
+            elseif obj:is_player() and obj:get_player_name() ~= self._owner then
+                obj:punch(self.object, 1.0, {
+                    full_punch_interval = 1.0,
+                    damage_groups = {fleshy = 3},
+                }, nil)
             end
-            self.timer = 0
+        end
+
+        if self._timer > 0.5 then
+            self.object:remove()
         end
     end
 })
