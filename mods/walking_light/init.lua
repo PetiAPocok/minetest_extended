b083f2c7a2f0fda062a6d23f13f6efcdbbf57395
PetiAPocok <mmandna@gmail.com>
2020-07-24 20:14:05 +0200

    walking light: decreased light removal's redundancy, added clear up functions

--- mods/walking_light/init.lua
+++ mods/walking_light/init.lua
@@ -1,5 +1,6 @@
 walking_light = {}
 local players = {}
+local timer = 0
 
 minetest.register_node("walking_light:light", {
     drawtype = "glasslike",
@@ -15,80 +16,71 @@ minetest.register_node("walking_light:light", {
     physical = false,
 })
 
-minetest.register_on_joinplayer(
-    function(player)
-        players[player:get_player_name()] = {}
-        players[player:get_player_name()]["old_pos"] = {}
-        players[player:get_player_name()]["current_pos"] = {}
-    end
-)
+minetest.register_on_joinplayer(function(player)
+    players[player:get_player_name()] = {}
+    players[player:get_player_name()]["old_pos"] = {}
+    players[player:get_player_name()]["current_pos"] = {}
+end)
 
-minetest.register_on_leaveplayer(
-    function(player)
-        players[player:get_player_name()] = nil
-    end
-)
+minetest.register_on_leaveplayer(function(player)
+    walking_light.remove_light(player:get_pos())
+    players[player:get_player_name()] = nil
+end)
 
-minetest.register_globalstep(function(dtime)
+minetest.register_on_dieplayer(function(player, reason)
+    walking_light.remove_light(player:get_pos())
+end)
+
+minetest.register_on_shutdown(function()
     for k,v in pairs(players) do
-        if minetest.get_player_by_name(k):get_wielded_item():get_definition().name == "default:torch" then
-            v["current_pos"] = vector.round(minetest.get_player_by_name(k):get_pos())
-            v["current_pos"]["y"] = v["current_pos"]["y"] + 1
+        if v["current_pos"]["y"] ~= nil then
+            walking_light.remove_light(v["current_pos"])
+        end
 
-            if v["old_pos"]["x"] ~= v["current_pos"]["x"] or v["old_pos"]["y"] ~= v["current_pos"]["y"] or v["old_pos"]["z"] ~= v["current_pos"]["z"] then
-                if v["old_pos"]["y"] ~= nil then
-                    walking_light.remove_light(v["old_pos"])
-                end
-
-                if minetest.get_node(v["current_pos"]).name == "air" then
-                    minetest.place_node(v["current_pos"], {name="walking_light:light"})
-                    v["old_pos"] = v["current_pos"]
-                end
-            end
-        else
-            if v["old_pos"]["y"] ~= nil then
-                walking_light.remove_light(v["old_pos"])
-            end
-            v["current_pos"] = {}
-            v["old_pos"] = {}
+        if v["old_pos"]["y"] ~= nil then
+            walking_light.remove_light(v["old_pos"])
         end
     end
 end)
 
-walking_light.remove_light = function(pos)
-    local area = {
-        {x=pos.x-1,y=pos.y-1,z=pos.z-1},
-        {x=pos.x-1,y=pos.y-1,z=pos.z},
-        {x=pos.x-1,y=pos.y-1,z=pos.z+1},
-        {x=pos.x-1,y=pos.y,z=pos.z-1},
-        {x=pos.x-1,y=pos.y,z=pos.z},
-        {x=pos.x-1,y=pos.y,z=pos.z+1},
-        {x=pos.x-1,y=pos.y+1,z=pos.z-1},
-        {x=pos.x-1,y=pos.y+1,z=pos.z},
-        {x=pos.x-1,y=pos.y+1,z=pos.z+1},
-        {x=pos.x,y=pos.y-1,z=pos.z-1},
-        {x=pos.x,y=pos.y-1,z=pos.z},
-        {x=pos.x,y=pos.y-1,z=pos.z+1},
-        {x=pos.x,y=pos.y,z=pos.z-1},
-        {x=pos.x,y=pos.y,z=pos.z},
-        {x=pos.x,y=pos.y,z=pos.z+1},
-        {x=pos.x,y=pos.y+1,z=pos.z-1},
-        {x=pos.x,y=pos.y+1,z=pos.z},
-        {x=pos.x,y=pos.y+1,z=pos.z+1},
-        {x=pos.x+1,y=pos.y-1,z=pos.z-1},
-        {x=pos.x+1,y=pos.y-1,z=pos.z},
-        {x=pos.x+1,y=pos.y-1,z=pos.z+1},
-        {x=pos.x+1,y=pos.y,z=pos.z-1},
-        {x=pos.x+1,y=pos.y,z=pos.z},
-        {x=pos.x+1,y=pos.y,z=pos.z+1},
-        {x=pos.x+1,y=pos.y+1,z=pos.z-1},
-        {x=pos.x+1,y=pos.y+1,z=pos.z},
-        {x=pos.x+1,y=pos.y+1,z=pos.z+1}
-    }
+minetest.register_globalstep(function(dtime)
+    if timer > 0.2 then
+        for k,v in pairs(players) do
+            if minetest.get_player_by_name(k):get_wielded_item():get_definition().name == "default:torch" then
+                v["current_pos"] = vector.round(minetest.get_player_by_name(k):get_pos())
+                v["current_pos"]["y"] = v["current_pos"]["y"] + 1
 
-    for _,v in ipairs(area) do
-        if minetest.get_node(v).name == "walking_light:light" then
-            minetest.remove_node(v)
+                if v["old_pos"]["x"] ~= v["current_pos"]["x"] or v["old_pos"]["y"] ~= v["current_pos"]["y"] or v["old_pos"]["z"] ~= v["current_pos"]["z"] then
+                    if v["old_pos"]["y"] ~= nil then
+                        walking_light.remove_light(v["old_pos"])
+                    end
+
+                    if minetest.get_node(v["current_pos"]).name == "air" then
+                        minetest.place_node(v["current_pos"], {name="walking_light:light"})
+                        v["old_pos"] = v["current_pos"]
+                    end
+                end
+            else
+                if v["old_pos"]["y"] ~= nil then
+                    walking_light.remove_light(v["old_pos"])
+                end
+                v["current_pos"] = {}
+                v["old_pos"] = {}
+            end
         end
+        timer = 0
+    end
+    timer = timer + dtime
+end)
+
+walking_light.remove_light = function(pos)
+    if minetest.get_node(pos).name == "walking_light:light" then
+        minetest.remove_node(pos)
+    end
+
+    pos.y = pos.y - 1
+
+    if minetest.get_node(pos).name == "walking_light:light" then
+        minetest.remove_node(pos)
     end
 end
