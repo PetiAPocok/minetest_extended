30b01879e4407483c4d54668223027f79d308769
PetiAPocok <mmandna@gmail.com>
2020-08-13 10:37:45 +0200

    Orb of electricity: renamed, made projectile longer, some cleanup.

--- mods/magic/orb_of_lightning/init.lua
+++ mods/magic/orb_of_lightning/init.lua
@@ -1,9 +1,9 @@
 local ray_count = 0
 local rays = {}
 
-minetest.register_craftitem("orb_of_electricity:orb_of_electricity", {
-    description = "Orb of Electricity\nMana: 10",
-    inventory_image = "orb_of_electricity_orb.png",
+minetest.register_craftitem("orb_of_lightning:orb_of_lightning", {
+    description = "Orb of Lightning\nMana: 10",
+    inventory_image = "orb_of_lightning_orb.png",
     on_use = function(itemstack, player, pointed_thing)
         local name = player:get_player_name()
         local players_mana = hbmana.get(name)
@@ -16,15 +16,25 @@ minetest.register_craftitem("orb_of_electricity:orb_of_electricity", {
             local objs = {}
             local loop_counter = 1
 
-            while loop_counter <= 30 do
+            while loop_counter <= 200 do
+                local clear = true
+
                 local entity_pos = {
                     x = player_pos.x + loop_counter * dir.x / 14.5,
                     y = player_pos.y + 1.4 + loop_counter * dir.y / 14.5,
                     z = player_pos.z + loop_counter * dir.z / 14.5
                 }
 
-                if minetest.get_node(entity_pos).name == "air" then
-                    objs[loop_counter] = minetest.add_entity(entity_pos, "orb_of_electricity:lightning")
+                local objs = minetest.get_objects_inside_radius(entity_pos, 1.5)
+
+                for k, obj in pairs(objs) do
+                    if obj:get_luaentity().name ~= "orb_of_lightning:lightning" and obj:get_luaentity().name ~= "__builtin:item" then
+                        clear = false
+                    end
+                end
+
+                if minetest.get_node(entity_pos).name == "air" and clear then
+                    objs[loop_counter] = minetest.add_entity(entity_pos, "orb_of_lightning:lightning")
 
                     objs[loop_counter]:set_rotation({
                         x = -player:get_look_vertical(),
@@ -35,7 +45,7 @@ minetest.register_craftitem("orb_of_electricity:orb_of_electricity", {
                     objs[loop_counter]:get_luaentity()._id = ray_count
                     objs[loop_counter]:get_luaentity()._owner = name
                 else
-                    loop_counter = 30
+                    loop_counter = 200
                 end
 
                 loop_counter = loop_counter + 1
@@ -48,7 +58,7 @@ minetest.register_craftitem("orb_of_electricity:orb_of_electricity", {
 })
 
 minetest.register_craft({
-	output = "orb_of_electricity:orb_of_electricity",
+	output = "orb_of_lightning:orb_of_lightning",
 	recipe = {
 		{"", "default:glass", ""},
 		{"default:glass", "default:mese_crystal", "default:glass"},
@@ -56,11 +66,11 @@ minetest.register_craft({
 	}
 })
 
-minetest.register_entity("orb_of_electricity:lightning", {
+minetest.register_entity("orb_of_lightning:lightning", {
     physical = false,
     visual = "mesh",
-    mesh = "orb_of_electricity_lightning.obj",
-    textures = {"orb_of_electricity_lightning.png"},
+    mesh = "orb_of_lightning_lightning.obj",
+    textures = {"orb_of_lightning_lightning.png"},
     collisionbox = {-0.01,-0.01,-0.07,0.01,0.01,0.01},
     pointable = false,
     visual_size = {x=1, y=1, z=1},
@@ -76,32 +86,10 @@ minetest.register_entity("orb_of_electricity:lightning", {
             self.object:remove()
         end
 
-        -- minetest.add_particlespawner({
-        --     amount = 10,
-        --     time = 0.10,
-        --     minpos = {
-        --         x = pos.x - 0.1,
-        --         y = pos.y - 0.1,
-        --         z = pos.z - 0.1
-        --     },
-        --     maxpos = {
-        --         x = pos.x + 0.1,
-        --         y = pos.y + 0.1,
-        --         z = pos.z + 0.1
-        --     },
-        --     minexptime = 0.1,
-        --     maxexptime = 0.2,
-        --     minsize = 0.50,
-        --     maxsize = 0.50,
-        --     collisiondetection = false,
-        --     vertical = false,
-        --     texture = "orb_of_electricity_particle.png",
-        -- })
-
         local objs = minetest.get_objects_inside_radius({x=pos.x,y=pos.y,z=pos.z}, 1.5)
         for k, obj in pairs(objs) do
             if obj:get_luaentity() ~= nil then
-                if obj:get_luaentity().name ~= "orb_of_electricity:lightning" and obj:get_luaentity().name ~= "__builtin:item" then
+                if obj:get_luaentity().name ~= "orb_of_lightning:lightning" and obj:get_luaentity().name ~= "__builtin:item" then
                     obj:punch(self.object, 1.0, {
                         full_punch_interval = 1.0,
                         damage_groups = {fleshy = 3},
