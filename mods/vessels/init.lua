548dc24f1d12493083248d74e03feea9bf511e0c
PetiAPocok <mmandna@gmail.com>
2020-08-22 14:26:14 +0200

    vessels: Removed unused items

--- mods/vessels/init.lua
+++ mods/vessels/init.lua
@@ -133,32 +133,6 @@ minetest.register_craft( {
 	}
 })
 
-minetest.register_node("vessels:drinking_glass", {
-	description = S("Empty Drinking Glass"),
-	drawtype = "plantlike",
-	tiles = {"vessels_drinking_glass.png"},
-	inventory_image = "vessels_drinking_glass_inv.png",
-	wield_image = "vessels_drinking_glass.png",
-	paramtype = "light",
-	is_ground_content = false,
-	walkable = false,
-	selection_box = {
-		type = "fixed",
-		fixed = {-0.25, -0.5, -0.25, 0.25, 0.3, 0.25}
-	},
-	groups = {vessel = 1, dig_immediate = 3, attached_node = 1},
-	sounds = default.node_sound_glass_defaults(),
-})
-
-minetest.register_craft( {
-	output = "vessels:drinking_glass 14",
-	recipe = {
-		{"default:glass", "", "default:glass"},
-		{"default:glass", "", "default:glass"},
-		{"default:glass", "default:glass", "default:glass"}
-	}
-})
-
 minetest.register_node("vessels:glass_jar", {
 	description = S("Empty Glass Jar"),
 	drawtype = "plantlike",
@@ -185,33 +159,6 @@ minetest.register_craft( {
 	}
 })
 
-minetest.register_node("vessels:steel_bottle", {
-	description = S("Empty Heavy Steel Bottle"),
-	drawtype = "plantlike",
-	tiles = {"vessels_steel_bottle.png"},
-	inventory_image = "vessels_steel_bottle.png",
-	wield_image = "vessels_steel_bottle.png",
-	paramtype = "light",
-	is_ground_content = false,
-	walkable = false,
-	selection_box = {
-		type = "fixed",
-		fixed = {-0.25, -0.5, -0.25, 0.25, 0.3, 0.25}
-	},
-	groups = {vessel = 1, dig_immediate = 3, attached_node = 1},
-	sounds = default.node_sound_defaults(),
-})
-
-minetest.register_craft( {
-	output = "vessels:steel_bottle 5",
-	recipe = {
-		{"default:steel_ingot", "", "default:steel_ingot"},
-		{"default:steel_ingot", "", "default:steel_ingot"},
-		{"", "default:steel_ingot", ""}
-	}
-})
-
-
 -- Glass and steel recycling
 
 minetest.register_craftitem("vessels:glass_fragments", {
@@ -231,15 +178,6 @@ minetest.register_craft( {
 minetest.register_craft( {
 	type = "shapeless",
 	output = "vessels:glass_fragments",
-	recipe = {
-		"vessels:drinking_glass",
-		"vessels:drinking_glass",
-	},
-})
-
-minetest.register_craft( {
-	type = "shapeless",
-	output = "vessels:glass_fragments",
 	recipe = { "vessels:glass_jar", "vessels:glass_jar"	},
 })
 
@@ -247,12 +185,6 @@ minetest.register_craft({
 	type = "cooking",
 	output = "default:glass",
 	recipe = "vessels:glass_fragments",
-})
-
-minetest.register_craft( {
-	type = "cooking",
-	output = "default:steel_ingot",
-	recipe = "vessels:steel_bottle",
 })
 
 minetest.register_craft({
