c58889fcc2569f0d81f5ccdf47c6fb682e3b6c3a
PetiAPocok <mmandna@gmail.com>
2020-10-04 20:05:41 +0200

    cement: fixed hard cement crash

--- mods/cement/init.lua
+++ mods/cement/init.lua
@@ -1,5 +1,4 @@
 -- Set textures for the different cement types
-
 local S = minetest.get_translator('cement')
 local dry_cement = 'default_sand.png^(default_gravel.png^[opacity:128)'
 local wet_cement = '('..dry_cement..')^[colorize:#000000:60'
@@ -7,7 +6,6 @@ local hard_cement = '('..wet_cement..')^[brighten'
 local sanded_cement = '(default_sand.png^(default_sandstone.png^[opacity:128)^[brighten)'
 
 -- Register dry cement and crafting recipe
-
 minetest.register_node('cement:dry', {
     description = S('Dry Cement'),
     tiles = { dry_cement },
@@ -23,7 +21,6 @@ minetest.register_craft({
 })
 
 -- Register wet cement and crafting recipe
-
 minetest.register_node('cement:wet', {
     description = S('Wet Cement'),
     tiles = { wet_cement },
@@ -47,21 +44,16 @@ minetest.register_craft({
 })
 
 -- Register hard cement
-
 minetest.register_node('cement:hard', {
     description = S('Hard Cement'),
     tiles = { hard_cement },
     groups = { cracky = 2},
     is_ground_content = false,
     sounds = default.node_sound_stone_defaults(),
-    on_punch = function (pos, node, puncher, pointed_thing)
-        colorize(pos, node, puncher, pointed_thing)
-    end
 })
 
 -- Register ABM to convert dry cement to wet cement when a water source
 -- is nearby
-
 minetest.register_abm({
     label = 'Turn dry cement to wet cement',
     nodenames = { 'cement:dry' },
@@ -76,7 +68,6 @@ minetest.register_abm({
 
 -- Register ABM to convert wet cement to hard cement when there is no
 -- water source nearby
-
 minetest.register_abm({
     label = 'Wet cement to hard cement',
     nodenames = { 'cement:wet' },
