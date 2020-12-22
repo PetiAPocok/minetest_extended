327835f78e76b3596e81e406b02a7a49fd344c3e
PetiAPook <mironm1996@gmail.com>
2020-12-22 10:55:30 +0100

    mex_moreblocks: Changed candle's texture, collusion, model height

--- mods/mex_moreblocks/candle.lua
+++ mods/mex_moreblocks/candle.lua
@@ -3,18 +3,24 @@ minetest.register_node("mex_moreblocks:candle", {
     drawtype = "mesh",
     mesh = "mex_moreblocks_candle.obj",
     tiles = {"mex_moreblocks_candle.png"},
+    -- inventory_image = 'mex_moreblocks_candle.png',
+    -- wield_image = 'mex_moreblocks_candle.png',
     use_texture_alpha = true,
     paramtype = "light",
     light_source = 12,
     sunlight_propagates = true,
     selection_box = {
-           type = "fixed",
-           fixed = {
-               {-0.2, -0.5, -0.2, 0.3, 0.1, 0.2},
-           },
+       type = "fixed",
+       fixed = {
+           {-0.2, -0.5, -0.2, 0.3, 0.1, 0.2},
        },
-    -- inventory_image = 'mex_moreblocks_candle.png',
-    -- wield_image = 'mex_moreblocks_candle.png',
+   },
+    collision_box = {
+        type = "fixed",
+        fixed = {
+            {0.0, 0.0, 0.0, 0.0, 0.0, 0.0},
+        },
+    },
     paramtype2 = "facedir",
     groups = {oddly_breakable_by_hand = 1},
 })
