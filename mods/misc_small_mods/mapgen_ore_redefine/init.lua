dc6ceda30835d4691722ef18a393913983f1da25
PetiAPook <mironm1996@gmail.com>
2020-12-31 14:55:28 +0100

    Modified ore depths

---
+++ mods/misc_small_mods/mapgen_ore_redefine/init.lua
@@ -0,0 +1,76 @@
+minetest.register_ore({
+    ore_type       = "scatter",
+    ore            = "default:stone_with_tin",
+    wherein        = "default:stone",
+    clust_scarcity = 12 * 12 * 12,
+    clust_num_ores = 4,
+    clust_size     = 3,
+    y_max          = -16,
+    y_min          = -2048,
+})
+
+minetest.register_ore({
+    ore_type       = "scatter",
+    ore            = "default:stone_with_copper",
+    wherein        = "default:stone",
+    clust_scarcity = 12 * 12 * 12,
+    clust_num_ores = 4,
+    clust_size     = 3,
+    y_max          = -16,
+    y_min          = -2048,
+})
+
+minetest.register_ore({
+    ore_type       = "scatter",
+    ore            = "default:stone_with_iron",
+    wherein        = "default:stone",
+    clust_scarcity = 12 * 12 * 12,
+    clust_num_ores = 29,
+    clust_size     = 5,
+    y_max          = -64,
+    y_min          = -2048,
+})
+
+minetest.register_ore({
+    ore_type       = "scatter",
+    ore            = "default:stone_with_gold",
+    wherein        = "default:stone",
+    clust_scarcity = 15 * 15 * 15,
+    clust_num_ores = 4,
+    clust_size     = 2,
+    y_max          = -128,
+    y_min          = -2048,
+})
+
+minetest.register_ore({
+    ore_type       = "scatter",
+    ore            = "default:stone_with_mese",
+    wherein        = "default:stone",
+    clust_scarcity = 18 * 18 * 18,
+    clust_num_ores = 4,
+    clust_size     = 2,
+    y_max          = -128,
+    y_min          = -2048,
+})
+
+minetest.register_ore({
+    ore_type       = "scatter",
+    ore            = "default:stone_with_diamond",
+    wherein        = "default:stone",
+    clust_scarcity = 17 * 17 * 17,
+    clust_num_ores = 4,
+    clust_size     = 3,
+    y_max          = -256,
+    y_min          = -2048,
+})
+
+minetest.register_ore({
+    ore_type       = "scatter",
+    ore            = "default:mese",
+    wherein        = "default:stone",
+    clust_scarcity = 36 * 36 * 36,
+    clust_num_ores = 3,
+    clust_size     = 2,
+    y_max          = -256,
+    y_min          = -2048,
+})
