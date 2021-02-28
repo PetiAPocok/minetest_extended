995a3f6ceab0fd36b54d8ac846859ad66b63a29d
PetiAPocok <mironm1996@gmail.com>
2021-02-28 22:10:31 +0100

    mex_morebeds: added new beds

--- mods/mex_morebeds/init.lua
+++ mods/mex_morebeds/init.lua
@@ -3,8 +3,6 @@ minetest.register_node("mex_morebeds:npc_bed", {
     drawtype = "mesh",
     mesh = "mex_morebeds_npc_bed.obj",
     tiles = {"mex_morebeds_npc_bed.png"},
-    -- inventory_image = 'mex_moreblocks_candle.png',
-    -- wield_image = 'mex_moreblocks_candle.png',
     selection_box = {
        type = "fixed",
        fixed = {
@@ -81,5 +79,78 @@ minetest.register_craft({
     recipe = {
         {"wool:green","wool:dark_green","wool:dark_green"},
         {"default:wood","default:wood","default:wood"},
+    }
+})
+
+
+minetest.register_node("mex_morebeds:aspen_bed", {
+    description = "Aspen Bed",
+    drawtype = "mesh",
+    mesh = "mex_morebeds_aspen_bed.obj",
+    tiles = {"mex_morebeds_aspen_bed.png"},
+    selection_box = {
+       type = "fixed",
+       fixed = {
+           {-0.5, -0.5, -1.24, 0.5, 0.0, 0.35},
+           {-0.55, -0.5, 0.3, 0.55, 0.32, 0.4},
+       },
+   },
+    collision_box = {
+        type = "fixed",
+        fixed = {
+            {-0.5, -0.5, -1.24, 0.5, 0.0, 0.35},
+            {-0.55, -0.5, 0.3, 0.55, 0.32, 0.4},
+        },
+    },
+    paramtype2 = "facedir",
+    groups = {oddly_breakable_by_hand = 1},
+
+    on_rightclick = function(pos, node, clicker, itemstack, pointed_thing)
+        beds.on_rightclick(pos, clicker)
+        return itemstack
+    end,
+})
+
+minetest.register_craft({
+    output = "mex_morebeds:aspen_bed",
+    recipe = {
+        {"wool:white","wool:white","wool:white"},
+        {"default:aspen_wood","default:aspen_wood","default:aspen_wood"},
+    }
+})
+
+minetest.register_node("mex_morebeds:jungle_bed", {
+    description = "Jungle Bed",
+    drawtype = "mesh",
+    mesh = "mex_morebeds_jungle_bed.obj",
+    tiles = {"mex_morebeds_jungle_bed.png"},
+    selection_box = {
+       type = "fixed",
+       fixed = {
+           {-0.5, -0.5, -1.24, 0.5, 0.0, 0.35},
+           {-0.55, -0.5, 0.25, 0.55, 0.85, 0.4},
+       },
+   },
+    collision_box = {
+        type = "fixed",
+        fixed = {
+            {-0.5, -0.5, -1.5, 0.5, 0.1, 0.4},
+            {-0.5, -0.5, 0.22, 0.5, 1.1, 0.4},
+        },
+    },
+    paramtype2 = "facedir",
+    groups = {oddly_breakable_by_hand = 1},
+
+    on_rightclick = function(pos, node, clicker, itemstack, pointed_thing)
+        beds.on_rightclick(pos, clicker)
+        return itemstack
+    end,
+})
+
+minetest.register_craft({
+    output = "mex_morebeds:jungle_bed",
+    recipe = {
+        {"wool:black","wool:black","wool:black"},
+        {"default:junglewood","default:junglewood","default:junglewood"},
     }
 })
