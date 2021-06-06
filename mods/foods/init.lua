968e111a026efa4f81011a29828fc54ad3e88004
PetiAPocok <mironm1996@gmail.com>
2021-06-06 11:52:36 +0200

    foods: Honeyed bread now gives back empty jar

--- mods/foods/init.lua
+++ mods/foods/init.lua
@@ -171,7 +171,10 @@ minetest.register_craftitem("foods:buttered_bread_with_honey", {
 minetest.register_craft({
     type = "shapeless",
     output = "foods:buttered_bread_with_honey 5",
-    recipe = {"foods:bread", "mobs:butter", "foods:jar_of_honey"}
+    recipe = {"foods:bread", "mobs:butter", "foods:jar_of_honey"},
+    replacements = {
+		{"foods:jar_of_honey", "vessels:glass_jar"}
+	}
 })
 
 -- Caramel
